ebf8e4a57f178ad3eb133aaf5268e40b6c0f2a38
Patryk Orłowski <orzeleagle122@gmail.com>
2021-06-07 11:08:11 +0200

    end styling

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,8 +1,27 @@
 import React from "react";
+import CartItem from "../components/CartItem";
 import "./CartScreen.css";
 
 const CartScreen = () => {
-  return <>cart screen</>;
+  return (
+    <>
+      <div className="cartscreen">
+        <div className="cartscreen__left">
+          <h2>Shopping Cart</h2>
+          <CartItem />
+        </div>
+        <div className="cartscreen__right">
+          <div className="cartscreen__info">
+            <p>Subtotal (0) items</p>
+            <p>$499.99</p>
+          </div>
+          <div>
+            <button>Proceed To Checkout</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CartScreen;
